672821f03ea9e5e82cd3bda9a757878c2d14acde
EstarG <gbaoxing@163.com>
2015-11-13 19:42:17 +0800

    a

---
+++ gbx.cpp
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <sstream>
+#include <stack>
+#include <map>
+#include <queue>
+ 
+#define CL(arr, val)    memset(arr, val, sizeof(arr))
+ 
+#define lc l,m,rt<<1
+#define rc m + 1,r,rt<<1|1
+#define pi acos(-1.0)
+#define ll __int64
+#define L(x)    (x) << 1
+#define R(x)    (x) << 1 | 1
+#define MID(l, r)   (l + r) >> 1
+#define Min(x, y)   (x) < (y) ? (x) : (y)
+#define Max(x, y)   (x) < (y) ? (y) : (x)
+#define E(x)        (1 << (x))
+#define iabs(x)     (x) < 0 ? -(x) : (x)
+#define OUT(x)  printf("%I64d\n", x)
+#define keyTree (chd[chd[root][1]][0])
+#define Read()  freopen("din.txt", "r", stdin)
+#define Write() freopen("dout.txt", "w", stdout);
+ 
+ 
+#define M 100007
+#define N 200017
+ 
+using namespace std;
+ 
+int dx[4]={-1,1,0,0};
+int dy[4]={0,0,-1,1};
+ 
+const int inf = 0x7f7f7f7f;
+const int mod = 1000000007;
+ 
+const double eps = 1e-8;
+
+int main() 
+{
+  
+}
